cd1d7111d1aaa12b158dae3ab44c79cb06646aa8
sairam0607 <srr6795@gmail.com>
2023-04-05 14:40:49 +0530

    Testing Pipeline

--- training.py
+++ training.py
@@ -1,3 +1,19 @@
 
 
 print("This is the training code!")
+
+print("This is the training code!")
+
+print("Learning Azure MLOPs!")    
+
+print("Testing Issues")
+
+print("Completing the Issue")
+
+print("Completed all Issues")
+
+print("Added the hyperparameter tuning to the training.py")
+
+print("Hyperparameter Tuning Bayseain Optimization- Completed")
+
+print("I want to test the Pipelines")
---
+++ util.py
@@ -0,0 +1,3 @@
+
+
+print('This is the util function')
